324e81f058adbd5b31f3a48aca55662b6951abc7
Paras <paras@intricare.net>
2021-12-14 10:11:21 +0530

    -test

--- app/src/main/java/com/contactninja/retrofit/RetrofitApiInterface.java
+++ app/src/main/java/com/contactninja/retrofit/RetrofitApiInterface.java
@@ -4,6 +4,8 @@ package com.contactninja.retrofit;
 import com.contactninja.Model.SignModel;
 import com.google.gson.JsonObject;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.internal.connection.Exchange;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -71,7 +73,7 @@ public interface RetrofitApiInterface {
     @POST("contact/import")
     Call<ApiResponse> Upload_csv(@Header("Accept") String api,
                                  @Header("Authorization") String auth,
-                                 @Part("items")RequestBody organization_id,
+                                 @Part("items") RequestBody organization_id,
                                  @Field("team_id") String team_id,
                                  @Field("user_id") String user_id,
                                  @Field("import_file") String import_file);
@@ -85,6 +87,7 @@ public interface RetrofitApiInterface {
             @Part("items") RequestBody items,
             @Part("isAny") RequestBody isAny
     );
+
 
 
 }
