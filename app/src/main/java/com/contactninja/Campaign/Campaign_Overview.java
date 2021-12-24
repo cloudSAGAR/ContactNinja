e3aeb0f6bd91ed464823c32066e8358de176dd3e
Shirish <shirish@intricare.net>
2021-12-24 14:57:49 +0530

    Desion 24

---
+++ app/src/main/java/com/contactninja/Campaign/Campaign_Overview.java
@@ -0,0 +1,55 @@
+package com.contactninja.Campaign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.contactninja.R;
+import com.contactninja.Utils.LoadingDialog;
+import com.contactninja.Utils.SessionManager;
+import com.contactninja.retrofit.RetrofitCalls;
+
+public class Campaign_Overview extends AppCompatActivity implements View.OnClickListener {
+    ImageView iv_back;
+    TextView save_button;
+    SessionManager sessionManager;
+    RetrofitCalls retrofitCalls;
+    LoadingDialog loadingDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_overview);
+        loadingDialog=new LoadingDialog(this);
+        sessionManager=new SessionManager(this);
+        retrofitCalls = new RetrofitCalls(this);
+        IntentUI();
+    }
+    private void IntentUI() {
+        iv_back=findViewById(R.id.iv_back);
+        iv_back.setVisibility(View.VISIBLE);
+        save_button=findViewById(R.id.save_button);
+        save_button.setVisibility(View.VISIBLE);
+        save_button.setOnClickListener(this);
+        iv_back.setOnClickListener(this);
+        save_button.setText("Next");
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.save_button:
+                //Add Api Call
+               // startActivity(new Intent(getApplicationContext(),Campaign_Overview.class));
+                break;
+
+        }
+    }
+}
--- app/src/main/java/com/contactninja/Campaign/First_Step_Start_Activity.java
+++ app/src/main/java/com/contactninja/Campaign/First_Step_Start_Activity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -147,7 +148,8 @@ public class First_Step_Start_Activity extends AppCompatActivity implements View
                 finish();
                 break;
             case R.id.save_button:
-                startActivity(new Intent(getApplicationContext(),First_Step_Start_Activity.class));
+                //Add Api Call
+                startActivity(new Intent(getApplicationContext(),Campaign_Overview.class));
                 break;
             case R.id.tv_use_tamplet:
                 bouttomSheet();
@@ -271,21 +273,13 @@ public class First_Step_Start_Activity extends AppCompatActivity implements View
             }else {
                 holder.line_view.setVisibility(View.GONE);
             }
+
             holder.tv_item.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    if(!item.isSelect()){
-                        Handler handler = new Handler();
-                        Runnable r = new Runnable() {
-                            @SuppressLint("NotifyDataSetChanged")
-                            public void run() {
-                                notifyDataSetChanged();
-                            }
-                        };
-                        handler.postDelayed(r, 1000);
-
-                        holder.tv_item.setTextColor(mCtx.getResources().getColor(R.color.white));
-                        interfaceClick.OnClick(item.getTemplateText());
+                public void onClick(View view) {
+                    if (holder.tv_item.getText().toString().equals("Save Current as template"))
+                    {
+                        showAlertDialogButtonClicked(view);
                     }
                 }
             });
@@ -309,6 +303,40 @@ public class First_Step_Start_Activity extends AppCompatActivity implements View
                 line_view=view.findViewById(R.id.line_view);
             }
         }
+    }
+
+    public void showAlertDialogButtonClicked(View view) {
+
+        // Create an alert builder
+        AlertDialog.Builder builder
+                = new AlertDialog.Builder(this, R.style.BottomSheetDialog);
+        final View customLayout
+                = getLayoutInflater()
+                .inflate(
+                        R.layout.add_titale_for_templet,
+                        null);
+        builder.setView(customLayout);
+        EditText editText = customLayout.findViewById(R.id.editText);
+        TextView tv_cancel = customLayout.findViewById(R.id.tv_cancel);
+        TextView tv_add = customLayout.findViewById(R.id.tv_add);
+        AlertDialog dialog
+                = builder.create();
+
+        dialog.show();
+        tv_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+            }
+        });
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
     }
 
 }
