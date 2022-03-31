bf78b9759884c9231684372f9638cbb00e1596f8
Paras <paras@intricare.net>
2022-03-31 16:44:02 +0530

    add no list text

--- app/src/main/java/com/contactninja/Manual_email_text/List_And_show/Item_List_Email_Detail_activty.java
+++ app/src/main/java/com/contactninja/Manual_email_text/List_And_show/Item_List_Email_Detail_activty.java
@@ -1411,6 +1411,17 @@ public class Item_List_Email_Detail_activty extends AppCompatActivity implements
                                 R.style.DialogStyle);
                         bottomSheetDialog.setContentView(mView);
                         RecyclerView rv_image_card = bottomSheetDialog.findViewById(R.id.rv_image_card);
+                        LinearLayout lay_no_list = bottomSheetDialog.findViewById(R.id.lay_no_list);
+
+                        if(bizcardList.size()!=0){
+                            rv_image_card.setVisibility(View.VISIBLE);
+                            lay_no_list.setVisibility(View.GONE);
+                        }else {
+                            rv_image_card.setVisibility(View.GONE);
+                            lay_no_list.setVisibility(View.VISIBLE);
+                        }
+
+
 
                         rv_image_card.setLayoutManager(new LinearLayoutManager(Item_List_Email_Detail_activty.this,
                                 LinearLayoutManager.HORIZONTAL, false));
